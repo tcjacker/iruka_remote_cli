8060f825fcf1fdf4ee962f03da6caf1605a971a5
TC JACK <tcjack@126.com>
2025-08-24 20:10:43 +0800

    bug fix

--- backend/app/api.py
+++ backend/app/api.py
@@ -97,7 +97,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token = auth.create_access_token(data={"sub": user.username})
+    access_token = auth.create_access_token(data={"sub": user.username},expires_delta=timedelta(days=1))
     return {"access_token": access_token, "token_type": "bearer"}
 
 
